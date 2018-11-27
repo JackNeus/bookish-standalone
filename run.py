32ee44219c198d93c948ddecb1e887af2cb32bd7
Jack Neus <jack.neus@gmail.com>
2018-11-26 22:35:24 -0500

    Tweaked run.py to work with gunicorn.

--- run.py
+++ run.py
@@ -2,16 +2,16 @@ from app import create_app
 from config import DevConfig, ProdConfig
 import sys
 
-if __name__== "__main__":
-	mode = "dev"
-	if len(sys.argv) > 1:
-		mode = sys.argv[1]
-	if mode == "prod":
-		config_class = ProdConfig
-	elif mode == "dev":
-		config_class = DevConfig
-	else:
-		exit("Invalid argument.")
-	app = create_app(config_class)
-	
+mode = "dev"
+if len(sys.argv) > 1:
+	mode = sys.argv[1]
+if mode == "prod":
+	config_class = ProdConfig
+elif mode == "dev":
+	config_class = DevConfig
+else:
+	exit("Invalid argument.")
+app = create_app(config_class)
+
+if __name__== "__main__":	
 	app.run(threaded=True)
