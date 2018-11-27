1c1de24f1c8f7841c67765525f9f08826b26b00b
Jack Neus <jack.neus@gmail.com>
2018-11-26 22:32:26 -0500

    Changed app init and config structure.

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 from app import create_app
-app = create_app()
+from config import DevConfig, ProdConfig
+import sys
 
-if __name__=="__main__":
-    app.run(debug = True, threaded=True)
+if __name__== "__main__":
+	mode = "dev"
+	if len(sys.argv) > 1:
+		mode = sys.argv[1]
+	if mode == "prod":
+		config_class = ProdConfig
+	elif mode == "dev":
+		config_class = DevConfig
+	else:
+		exit("Invalid argument.")
+	app = create_app(config_class)
+	
+	app.run(threaded=True)
