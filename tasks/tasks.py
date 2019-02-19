9f7ac97de83381a8cf8a9b83b486b0597478a1a9
Jack Neus <jack.neus@gmail.com>
2019-02-19 11:31:46 -0500

    Initial work on bigram backend.

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,5 +1,5 @@
 # This file will contain all possible background jobs that the user can run.
-
+import nltk
 import math
 import os
 import requests
@@ -8,6 +8,7 @@ import subprocess
 import queue
 from collections import defaultdict
 from itertools import repeat
+from nltk.corpus import stopwords
 from rq import get_current_job
 from flask import current_app
 
@@ -18,6 +19,8 @@ def resolve_task(task_name):
         return ucsf_api_aggregate_task
     if task_name == "ngram" or task_name == "word_freq_task":
         return word_freq_task
+    if task_name == "top_bigrams":
+        return get_top_bigrams_task
     return None
 
 # Get full results for a UCSF IDL API request.
@@ -159,6 +162,73 @@ def get_word_freq(file_data, keywords):
     push_metadata_to_db("files_analyzed")
     return (fileyear, freqs, len(file))
 
+def get_top_bigrams_task(file_list_path):
+    init_job([file_list_path])
+
+    # TODO: Make sure file_list_path is a valid task ID.
+    file_list_file = open(config["TASK_RESULT_PATH"] + file_list_path)
+    def extract(line):
+        line = line.split(" ")
+        line[0] += ".clean"
+        if len(line) > 1:
+            line[1] = int(line[1][:-1])
+        return tuple(line)
+
+    file_list = map(lambda line: extract(line), file_list_file.readlines())
+    # Remove files without years.
+    file_list = list(filter(lambda x: len(x) > 1, file_list))
+
+    set_task_size(len(file_list))
+    set_task_metadata("files_analyzed", 0)
+    return_from_task(get_top_bigrams(file_list))
+
+def get_top_bigrams(files):
+    to_process = len(files)
+
+    bigram_freqs = get_pool().starmap(get_bigrams, files)
+    # Currently, discard any files that couldn't be found.
+    bigram_freqs = [x for x in word_freqs if x is not None]
+    set_task_metadata("files_analyzed", len(word_freqs))
+
+    # Merge dictionaries.
+    years = [x[1] for x in files]
+    min_year = int(min(years))
+    max_year = int(max(years))
+    years = range(min_year, max_year+1)
+
+    global_freqs = init_dict(years, defaultdict(lambda x: 0, {}))
+
+    for year, freqs in bigram_freqs:
+        for bigram, freq in freqs:
+            global_freqs[year][bigram] += freq
+    global_freqs = {k:v for (k,v) in d.items() if v >= 20}
+
+    return global_freqs
+
+
+def get_bigrams(file_data):
+    filename, fileyear = file_data
+
+    try:
+        file = open(filename, "r")
+    except FileNotFoundError as e:
+        return None
+    
+    file = list(map(lambda x: x.strip(), file.readlines()))
+    bigrams = defaultdict(lambda: 0, {})
+    stop_words = set(stopwords.words('english'))
+    for i in range(len(file) - 1):
+        if file[i+1] in stop_words:
+            i += 1
+            continue
+        if file[i] in stop_words:
+            continue
+        bigram = tuple(sorted((file[i], file[i+1])))
+        bigrams[bigram] += 1
+
+    return (fileyear, bigrams)
+
+
 
 # This job will probably never be client-facing, as it only needs to be run once.
 def clean(files):
@@ -173,10 +243,12 @@ def clean(files):
 '''
 # Temporary. Comment this out before running the actual app.
 txt = []
+i = 0
 for subdir, dirs, files in os.walk("../txt/ucsf/"):
     for file in files:
-        txt.append(subdir + "/" + file)
-
-clean(txt + txt)
+        txt.append((subdir + "/" + file, 1900 + i % 10))
+        i += 1
+print(txt[:10])
+print(get_top_bigrams(txt))
 print("Done.")
 '''
