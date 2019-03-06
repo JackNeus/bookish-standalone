0105ec874efce38791cebde707a7d012db0dcf1c
Jack Neus <jack.neus@gmail.com>
2019-03-05 22:45:22 -0500

    Forgot to readd code.

--- app/static/js/bigram_viewer.js
+++ app/static/js/bigram_viewer.js
@@ -107,4 +107,27 @@ function zoom_actions(){
     svg.attr("transform", d3.event.transform)
 }
 
-render_chart(task_results);
+var convert_data = function(year) {
+  var data = task_results[year];
+  var node_set = new Set([]);
+  var links = [];
+  data.forEach(function(element) {
+    node_set.add(element[0][0]);
+    node_set.add(element[0][1]);
+    links.push({
+      "source": element[0][0],
+      "target": element[0][1],
+      "value": element[1]
+    });
+  });
+  var nodes = [];
+  node_set.forEach(function(element) {
+    nodes.push({
+      "id": element,
+      "group": 1
+    });
+  });
+  return {"nodes": nodes, "links": links};
+}
+
+render_chart(convert_data(1905));
