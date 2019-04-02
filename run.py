c01048c12ab735e7a9ce588cb34e8810295f66b3
JackNeus <jack.neus@gmail.com>
2019-04-02 15:24:33 -0400

    Fixed issue with os environ var.

--- run.py
+++ run.py
@@ -10,13 +10,12 @@ def build_app(mode):
 		config_class = DevConfig
 	else:
 		exit("Invalid argument.")
-
+	os.environ["in_bookish"] = "true"
 	return create_app(config_class)
 
 if __name__== "__main__":	
 	mode = "dev"
 	if len(sys.argv) > 1:
 		mode = sys.argv[1]
-	os.environ["in_bookish"] = "true"
 	app = build_app(mode)
 	app.run(threaded=True)
