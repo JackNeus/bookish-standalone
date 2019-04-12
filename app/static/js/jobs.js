5bd42350fd94bf3b6efa6924145a669c37eaa16a
Jack Neus <jack.neus@gmail.com>
2019-04-12 14:36:05 -0400

    Truncate description if too long.

--- app/static/js/jobs.js
+++ app/static/js/jobs.js
@@ -1,33 +1,44 @@
 setInterval(function() {
-	$.getJSON("/jobs.json").done(function(data) {
-		for (let i = 0; i < data.length; i++) {
-			let job = data[i];
-			let job_table_row = $("tr#"+job.id);
-
-			job_table_row.find("td.status").html(job.status);
-			job_table_row.find("td.task-progress").html(job.progress);
-			job_table_row.find("td.description").html(job.description);
-			job_table_row.find("td.time-started").html(job.time_started);
-			job_table_row.find("td.time-finished").html(job.time_finished);
-			
-			if (job.status === "Completed") {
-				job_table_row.find("td.view-results").html("<a href='jobs/view/"+job.id+"'>View Results</a>");
-			} else {
-				job_table_row.find("td.view-results").html("");
-			}
-
-			if (job.status === "Queued" || job.status === "Running") {
-				job_table_row.find("td.action").html("<a href='jobs/kill/"+job.id+"'>Abort Job</a>");
-			} else {
-				job_table_row.find("td.action").html("<a href='jobs/delete/"+job.id+"'>Delete Job</a>");
-				job_table_row.find("td.replay").html("<a href='jobs/replay/"+job.id+"'>Replay</a>");
-			}
-				
-		}
-		updateLinks();
-		updateRows();
-	});
+	$.getJSON("/jobs.json").done(function(data) { updateData(data); });
 }, 1500);
+
+function updateData(data) {
+	for (let i = 0; i < data.length; i++) {
+		let job = data[i];
+		let job_table_row = $("tr#"+job.id);
+
+		job_table_row.find("td.status").html(job.status);
+		job_table_row.find("td.task-progress").html(job.progress);
+		var description = job.description;
+
+		// Truncate description if too long.
+		var max_description_length = 150;
+		if (job.description.length > max_description_length) {
+			description = job.description.substr(0, max_description_length) + "...";
+		}
+		job_table_row.find("td.description")
+			.html(description)
+			.attr("title", job.description);
+		job_table_row.find("td.time-started").html(job.time_started);
+		job_table_row.find("td.time-finished").html(job.time_finished);
+		
+		if (job.status === "Completed") {
+			job_table_row.find("td.view-results").html("<a href='jobs/view/"+job.id+"'>View Results</a>");
+		} else {
+			job_table_row.find("td.view-results").html("");
+		}
+
+		if (job.status === "Queued" || job.status === "Running") {
+			job_table_row.find("td.action").html("<a href='jobs/kill/"+job.id+"'>Abort Job</a>");
+		} else {
+			job_table_row.find("td.action").html("<a href='jobs/delete/"+job.id+"'>Delete Job</a>");
+			job_table_row.find("td.replay").html("<a href='jobs/replay/"+job.id+"'>Replay</a>");
+		}
+			
+	}
+	updateLinks();
+	updateRows();
+}
 
 function updateRows() {
 	$(".select").on("click", function() {
@@ -87,8 +98,3 @@ function updateLinks() {
 		});
 	}
 }
-
-
-
-updateRows();
-updateLinks();
