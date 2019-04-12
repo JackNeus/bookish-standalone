3fb2eba178a1b9f7b72708e23525d86fafd8efe8
Jack Neus <jack.neus@gmail.com>
2019-04-12 14:16:11 -0400

    Fixed abort bug.

--- app/static/js/jobs.js
+++ app/static/js/jobs.js
@@ -25,3 +25,67 @@ setInterval(function() {
 		}
 	});
 }, 1500);
+
+function updateRows() {
+	$(".select").on("click", function() {
+		updateLinks();
+	})
+}
+
+function updateLinks() {
+	$(".view-results").html("");
+	$(".action").html("");
+
+	var selected_ids = [];
+	var task_type;
+	var valid_vis = true;
+	var all_deleteable = true;
+	var selected_rows = $(".select:checked").parent().parent().each(function() {
+		let status = $(this).find(".status").text();
+		let type = $(this).find(".id").text();
+		if (task_type === undefined)
+			task_type = type;
+		else if (task_type !== type || status !== "Completed")
+			valid_vis = false;
+		selected_ids.push(this.id);
+
+		if (status === "Queued" || status === "Running" || status === "Aborting") 
+			all_deleteable = false;
+	});
+
+	if (selected_ids.length > 0) {
+		var url_param = selected_ids.join(";");
+
+		if (valid_vis && (selected_ids.length == 1 || task_type === "word_family_graph_task")) {
+			// Currently, only support multivisualization of word family.
+			$(".select:checked").parent().parent().find(".view-results")
+				.html("<a href=\"jobs/view/"+url_param+"\">View Results</a>")
+		}
+		if (all_deleteable) {
+			let action_text = selected_ids.length > 1 ? "Delete Jobs" : "Delete Job";
+			$(".select:checked").parent().parent().find(".action")
+				.html("<a href=\"jobs/delete/"+url_param+"\">"+action_text+"</a>");
+		}
+	} else {
+		$("tr").each(function() {
+			var id = $(this)[0].id;
+			$(this).find(".view-results")
+				.html("<a href=\"jobs/view/"+id+"\">View Results</a>")
+			var status = $(this).find(".status").text();
+			if (status === "Queued" || status === "Running") {
+				$(this).find(".action")
+					.html("<a href=\"jobs/kill/"+id+"\">Abort Job</a>");
+			} else if (status !== "Aborting") {
+				$(this).find(".action")
+					.html("<a href=\"jobs/delete/"+id+"\">Delete Job</a>");
+
+			}
+		});
+	}
+}
+
+
+
+updateRows();
+updateLinks();
+>>>>>>> Stashed changes
