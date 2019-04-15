ddac5cda3a6b9b71cce93e1545f304bc10ced8d8
Jack Neus <jack.neus@gmail.com>
2019-04-15 12:47:35 -0400

    Reordered ngram colors.

--- app/static/js/ngram_viewer.js
+++ app/static/js/ngram_viewer.js
@@ -7,24 +7,24 @@ var x_max = 0;
 
 var free_colors = new Set([
 "#1f77b4",
-"#ff7f0e",
-"#2ca02c",
-"#d62728",
-"#9467bd",
-"#8c564b",
-"#e377c2",
-"#7f7f7f",
-"#bcbd22",
-"#17becf",
 "#aec7e8",
+"#ff7f0e",
 "#ffbb78",
+"#2ca02c",
 "#98df8a",
+"#d62728",
 "#ff9896",
+"#9467bd",
 "#c5b0d5",
+"#8c564b",
 "#c49c94",
+"#e377c2",
 "#f7b6d2",
+"#7f7f7f",
 "#c7c7c7",
+"#bcbd22",
 "#dbdb8d",
+"#17becf",
 "#9edae5"]);
 var used_colors = new Set();
 
