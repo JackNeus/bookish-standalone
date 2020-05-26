62dd9ce0dd3d84193ca6884acef74e314923010c
Jack Neus <jack.neus@gmail.com>
2020-05-26 14:30:52 -0400

    Added bigrams and word_family to CLI

--- tasks/analyze.py
+++ tasks/analyze.py
@@ -3,10 +3,13 @@ import argparse
 from collections import defaultdict
 import copy
 from itertools import repeat, starmap
-import json
+import math
 import os
 import sys
 import time
+
+from multi_util import *
+from stopwords import stopwords
 
 
 def is_valid_file(parser, arg):
@@ -64,14 +67,17 @@ word_freq_parser.add_argument("keywords",
                               nargs="+",
                               help='Keywords to analyze (case-insensitive)')
 
+bigram_parser = subparsers.add_parser('bigram', help='Top Bigrams Analysis')
 
-# We use this function to concisely define multi-level dictionaries properly
-# (the naive approach copies the inner dictionary by reference instead of value)
-def init_dict(keys, default_value):
-    new_dict = {}
-    for k in keys:
-        new_dict[k] = copy.deepcopy(default_value)
-    return new_dict
+word_family_parser = subparsers.add_parser('word_family',
+                                           help='Word Family Graph Analysis')
+word_family_parser.add_argument(
+    metavar="word_family",
+    dest="word_families",
+    nargs="+",
+    help='Word families to analyze -- words in a family are comma separated')
+
+# ---------- WORD FREQ ----------
 
 
 # Generic word freq function.
@@ -135,17 +141,206 @@ def get_word_freq(file_data, keywords):
     return (fileyear, freqs, len(file))
 
 
-# Handy little recursive function for serializing result objects.
-def dump_task_results(data):
-    if isinstance(data, list):
-        result = ""
-        for row in data:
-            result += dump_task_results(row) + '\n'
-        return result
-    elif isinstance(data, dict):
-        return json.dumps(data)
-    else:
-        return str(data) + '\n'
+# ---------- BIGRAM ----------
+
+
+def get_top_bigrams(files):
+    bigram_freqs = partition_map(get_bigrams, [x[::-1] for x in files])
+    # Currently, discard any files that couldn't be found.
+    bigram_freqs = [x for x in bigram_freqs if x is not None]
+
+    # Merge dictionaries.
+    years = [x[1] for x in files]
+
+    global_freqs = init_dict(years, defaultdict(lambda: 0, {}))
+    total_bigram_counts = init_dict(years, 0)
+    metadata = init_dict(years, defaultdict(lambda: 0, {}))
+    for year in years:
+        metadata[year]["Files Analyzed"] += 1
+
+    for year, freqs, file_size in bigram_freqs:
+        total_bigram_counts[year] += file_size**2
+        for bigram, freq in freqs.items():
+            global_freqs[year][bigram] += freq
+        metadata[year]["Total Word Count"] += file_size
+    for year in global_freqs.keys():
+        # Only return bigrams that make up more than .5% of all bigrams for that year.
+        #freq_threshold = 0.005 * total_bigram_counts[year]
+        #freq_threshold = 25
+        #global_freqs[year] = {";".join(k):v for (k,v) in global_freqs[year].items() if v >= freq_threshold}
+        global_freqs[year] = n_highest_entries(global_freqs[year], 50)
+
+    return [global_freqs, metadata]
+
+
+def n_highest_entries(d, n):
+    if isinstance(d, dict):
+        d = d.items()
+    elif not isinstance(d, list):
+        return None
+    return sorted(d, key=lambda x: x[1], reverse=True)[:n]
+
+
+def get_bigrams(files, year):
+    # Still works with singletons.
+    if not isinstance(files, list):
+        files = [files]
+
+    bigrams = defaultdict(lambda: 0, {})
+    file_length = 0
+
+    for filename in files:
+        try:
+            file = open(filename, "r")
+        except FileNotFoundError as e:
+            continue
+
+        file_bigrams = defaultdict(lambda: 0)
+        # TODO: Make .lower() a parameter.
+        file = list(map(lambda x: x.strip().lower(), file.readlines()))
+        for i in range(len(file) - 1):
+            if file[i + 1] in stopwords:
+                i += 1
+                continue
+            if file[i] in stopwords:
+                continue
+            bigram = tuple((file[i], file[i + 1]))
+            file_bigrams[bigram] += 1
+
+        # Only take the top 50 bigrams for an individual file.
+        file_bigrams = n_highest_entries(file_bigrams, 50)
+        # Merge dictionaries.
+        for key, value in file_bigrams:
+            bigrams[key] += value
+
+        file_length += len(file)
+    return (year, dict(bigrams), file_length)
+
+
+# ---------- WORD FAMILY ----------
+
+
+def get_word_family_graph(file_list, word_families):
+    keywords = []
+    if isinstance(word_families, list):
+        temp = {}
+        for i in range(len(word_families)):
+            temp[i] = word_families[i]
+        word_families = temp
+    for family in word_families.values():
+        keywords = keywords + family
+    # Remove stopwords.
+    keywords = filter(lambda x: x not in stopwords, keywords)
+    # Remove duplicates.
+    keywords = list(set(keywords))
+
+    word_family_data = starmap(get_word_family_data,
+                               zip(file_list, repeat(keywords)))
+
+    # Merge dictionaries.
+    years = [x[1] for x in file_list]
+
+    empty_fcm = defaultdict(lambda: copy.deepcopy(defaultdict(lambda: 0)))
+    fcms = init_dict(years, empty_fcm)
+    word_freqs = init_dict(years, defaultdict(lambda: 0, []))
+    metadata = init_dict(years, defaultdict(lambda: 0, []))
+
+    # Merge fcms by year.
+    for entry in word_family_data:
+        if entry is None:
+            continue
+        year, file_fcm, file_word_freqs, word_count = entry
+        metadata[year]["Files Analyzed"] += 1
+        metadata[year]["Total Word Count"] += word_count
+        for keyword in file_fcm:
+            word_freqs[year][keyword] += file_word_freqs[keyword]
+            for word, gfreq in file_fcm[keyword].items():
+                fcms[year][keyword][word] += gfreq
+
+    # Convert from defaultdicts to dicts.
+    fcms = dict(fcms)
+    word_freqs = dict(word_freqs)
+    metadata = dict(metadata)
+    for year in fcms:
+        word_freqs[year] = dict(word_freqs[year])
+        fcms[year] = dict(fcms[year])
+        metadata[year] = dict(metadata[year])
+        for keyword in fcms[year]:
+            fcms[year][keyword] = dict(fcms[year][keyword])
+
+    # Normalize word freq table to [0, 1].
+    for year in word_freqs:
+        if len(word_freqs[year]) > 0:
+            min_freq = min(word_freqs[year].values())
+            max_freq = max(word_freqs[year].values())
+            for word, freq in word_freqs[year].items():
+                freq_range = max_freq - min_freq
+                if max_freq == min_freq:
+                    freq_range = 1
+                word_freqs[year][word] = (freq - min_freq) / freq_range
+
+    # Adjust weights in fcms
+    for year in fcms:
+        max_edge_val = 0
+        # weight = log(1 + weight)
+        for keyword in fcms[year]:
+            for word, val in fcms[year][keyword].items():
+                fcms[year][keyword][word] = math.log(1 + val)
+                max_edge_val = max(max_edge_val, fcms[year][keyword][word])
+        # normalize so <= 1
+        if max_edge_val != 0:
+            for keyword in fcms[year]:
+                for word, val in fcms[year][keyword].items():
+                    fcms[year][keyword][word] = val / max_edge_val
+
+    return [fcms, word_freqs, word_families, metadata]
+
+
+def get_word_family_data(file_data, keywords):
+    filename, fileyear = file_data
+
+    # TODO: Determine behavior when a file can't be found.
+    try:
+        file = open(filename, "r")
+    except FileNotFoundError as e:
+        print("Could not find %s. Skipping..." % filename)
+        return None
+    # TODO: Remove call to lower()
+    file = list(map(lambda x: x.strip().lower(), file.readlines()))
+
+    sigma = 5
+    window_size = 4 * sigma
+    weights = [
+        math.exp((-x**2) / (2 * sigma)) / sigma
+        for x in range(0, window_size + 1)
+    ]
+
+    # Only calculate fcm for keywords that appear in the file.
+    keywords = list(filter(lambda x: x in file, keywords))
+
+    # Compute feature co-ocurrence matrix using a
+    # Gaussian weighting of word frequencies.
+    fcm = init_dict(keywords, {x: 0 for x in keywords})
+    # Also, build a frequency table for the keywords.
+    word_freq = init_dict(keywords, 0)
+    for i in range(len(file)):
+        if file[i] not in keywords:
+            continue
+        word_freq[file[i]] += 1
+        for j in range(max(0, i - window_size),
+                       min(len(file), i + window_size + 1)):
+            # Don't want to compare word to itself.
+            if i == j:
+                continue
+            if file[j] in keywords:
+                fcm[file[i]][file[j]] += weights[abs(i - j)]
+                # Avoid double counting if the words are the same.
+                if file[i] != file[j]:
+                    fcm[file[j]][file[i]] += weights[abs(i - j)]
+    return (fileyear, fcm, word_freq, len(file))
+
+
+# ---------- MISC ----------
 
 
 def main():
@@ -169,6 +364,11 @@ def main():
     res = None
     if args.task == "word_freq":
         res = word_freq(file_list, args.keywords)
+    if args.task == "bigram":
+        res = get_top_bigrams(file_list)
+    if args.task == "word_family":
+        word_families = [x.split(",") for x in args.word_families]
+        res = get_word_family_graph(file_list, word_families)
 
     output_file = open(output_filename, "w")
     output_file.write(dump_task_results(res))
--- tasks/multi_util.py
+++ tasks/multi_util.py
@@ -1,32 +1,71 @@
 from collections import defaultdict
+from itertools import starmap
+import json
+
+
+# We use this function to concisely define multi-level dictionaries properly
+# (the naive approach copies the inner dictionary by reference instead of value)
+def init_dict(keys, default_value):
+    new_dict = {}
+    for k in keys:
+        new_dict[k] = copy.deepcopy(default_value)
+    return new_dict
+
+
+# Handy little recursive function for serializing result objects.
+def dump_task_results(data):
+    if isinstance(data, list):
+        result = ""
+        for row in data:
+            result += dump_task_results(row) + '\n'
+        return result
+    elif isinstance(data, dict):
+        return json.dumps(data)
+    else:
+        return str(data) + '\n'
+
+
+def partition_map(map_func,
+                  items,
+                  size_func=lambda *a: 1,
+                  max_partition_size=100):
+    items = [item + (size_func(item), ) for item in items]
+    partitions = partition(items, max_partition_size)
+    # In theory, can be replaced with multiprocessing.starmap
+    mapped_values = starmap(map_func, partitions)
+    return mapped_values
+
+    # Calculate max_partition_size in bytes.
+    #bytes_per_gb = 1024 * 1024 * 1024
+    #usable_memory = 1 * bytes_per_gb
+    #max_partition_size = usable_memory / config["NUM_CORES"]
+
 
 # items is a list of (key, value, size)
 # will return a list of tuples of the form ([value1, value2, ...], key)
 def partition(items, max_partition_size):
-	partitions = []
+    partitions = []
 
-	current_partition = list()
-	current_partition_key = None
-	current_partition_size = 0
+    current_partition = list()
+    current_partition_key = None
+    current_partition_size = 0
 
-	items.sort(key = lambda x: x[0])
-	for item in items:
-		key, value, size = item
-		# This guarantees that even items with size > max_partition_size will
-		# make it into a partition.
-		if key != current_partition_key or current_partition_size + size > max_partition_size:
-			if len(current_partition) > 0:
-				partitions.append((current_partition, current_partition_key))
-			current_partition = [value]
-			current_partition_key = key
-			current_partition_size = size
-		else:
-			current_partition.append(value)
-			current_partition_size += size
+    items.sort(key=lambda x: x[0])
+    for item in items:
+        key, value, size = item
+        # This guarantees that even items with size > max_partition_size will
+        # make it into a partition.
+        if key != current_partition_key or current_partition_size + size > max_partition_size:
+            if len(current_partition) > 0:
+                partitions.append((current_partition, current_partition_key))
+            current_partition = [value]
+            current_partition_key = key
+            current_partition_size = size
+        else:
+            current_partition.append(value)
+            current_partition_size += size
 
-	if len(current_partition) > 0:
-		partitions.append((current_partition, current_partition_key))
+    if len(current_partition) > 0:
+        partitions.append((current_partition, current_partition_key))
 
-	return partitions
-
-# TODO: unit tests
+    return partitions
